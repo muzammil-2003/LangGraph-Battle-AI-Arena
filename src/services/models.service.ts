ef6e1e8ab99b52973931ca0c3b32073487c00a9b
Muzammil Ahmed Mangrio <ahmedmuzammil144@gmail.com>
2026-07-02 11:58:32 +0500

    feat: add new AI models and graph service
    - Updated package.json to include new dependencies: @langchain/cohere, @langchain/langgraph, and @langchain/mistralai.
    - Enhanced models.service.ts to export new Mistral and Cohere models alongside the existing Gemini model.
    - Created a new graph.ai.service.ts file to manage AI battle state and judgement logic.

--- src/services/models.service.ts
+++ src/services/models.service.ts
@@ -1,7 +1,19 @@
 import { ChatGoogle } from "@langchain/google";
+import { ChatMistralAI } from "@langchain/mistralai";
+import { ChatCohere, Cohere } from "@langchain/cohere"
 import config from "../config/config.js";
 
-const geminiModel = new ChatGoogle({
+export const geminiModel = new ChatGoogle({
     model: "gemini-2.5-flash-lite",
     apiKey: config.GOOGLE_API_KEY
 });
+
+export const mistralModel = new ChatMistralAI({
+    model: "mistral-small-latest",
+    apiKey: config.MISTRAL_API_KEY
+})
+
+export const cohereModel = new ChatCohere({
+    model: "command-r-03-2024",
+    apiKey: config.COHERE_API_KEY
+})
